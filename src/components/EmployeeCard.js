f87125106da2d58f877cd7b3cb142bb03fe29774
Pavitar Kumar <pavitrsheoran@gmail.com>
2021-10-11 16:40:33 +0530

    Modified text-size

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -47,11 +47,11 @@ function EmployeeCard(props) {
     if (member.isCeo) {
         return (
             <div className="my-6 mx-auto bg-yellow-300 shadow-lg rounded-xl w-3/5 grid grid-flow-rows sm:grid-rows-2 lg:grid-flow-col px-auto py-2 align-middle">
-                <p className="text-center my-4 text-xs  lg:border-black lg:border-r font-bold col-span-1">{member.name}</p>
-                <p className="text-center my-4 text-xs lg:border-black lg:border-r font-bold col-span-1">{`CEO`}</p>
-                <p className="text-center my-4 text-xs lg:border-black lg:border-r font-bold col-span-1">{member.id}</p>
-                <p className="text-center my-4 text-xs lg:border-black lg:border-r font-bold col-span-1">{member.email}</p>
-                <p className="text-center my-4 text-xs font-bold">{member.contact}</p>
+                <p className="text-center my-4 text-base  lg:border-black lg:border-r font-bold col-span-1">{member.name}</p>
+                <p className="text-center my-4 text-base lg:border-black lg:border-r font-bold col-span-1">{`CEO`}</p>
+                <p className="text-center my-4 text-base lg:border-black lg:border-r font-bold col-span-1">{member.id}</p>
+                <p className="text-center my-4 text-base lg:border-black lg:border-r font-bold col-span-1">{member.email}</p>
+                <p className="text-center my-4 text-base font-bold">{member.contact}</p>
                 {!props.isSearching ? <button className="bg-blue-400 w-20 px-2 py-1 rounded-md active:bg-blue-500 focus:ring-2 focus:ring-blue-600 m-auto" onClick={editEmployee}>Edit</button> : null}
             </div >
         )
@@ -60,11 +60,11 @@ function EmployeeCard(props) {
         // For department Heads
         return (
             <div className="my-6 mx-auto bg-yellow-300 shadow-lg rounded-xl w-3/5 grid grid-flow-rows sm:grid-rows-2 lg:grid-flow-col px-auto py-2 align-middle">
-                <p className="text-center my-4 text-xs  lg:border-black lg:border-r col-span-1">{member.name}</p>
-                <p className="text-center my-4 text-xs lg:border-black lg:border-r col-span-1">{`Head of ${member.department}`}</p>
-                <p className="text-center my-4 text-xs lg:border-black lg:border-r col-span-1">{member.id}</p>
-                <p className="text-center my-4 text-xs lg:border-black lg:border-r col-span-1">{member.email}</p>
-                <p className="text-center my-4 text-xs">{member.contact}</p>
+                <p className="text-center my-4 text-base  lg:border-black lg:border-r col-span-1">{member.name}</p>
+                <p className="text-center my-4 text-base lg:border-black lg:border-r col-span-1">{`Head of ${member.department}`}</p>
+                <p className="text-center my-4 text-base lg:border-black lg:border-r col-span-1">{member.id}</p>
+                <p className="text-center my-4 text-base lg:border-black lg:border-r col-span-1">{member.email}</p>
+                <p className="text-center my-4 text-base">{member.contact}</p>
                 <div className="flex justify-center items-center">
                     {!props.isSearching ? <button className="bg-blue-400 px-2 py-1 w-20 rounded-md active:bg-blue-500 focus:ring-2 focus:ring-blue-600 m-auto" onClick={editEmployee}>Edit</button> : null}
                 </div>
@@ -73,18 +73,18 @@ function EmployeeCard(props) {
     } else {
         return (
             <div className="my-6 mx-auto bg-yellow-300 shadow-lg rounded-xl w-4/5 grid grid-flow-rows sm:grid-rows-2 lg:grid-flow-col px-auto py-2 align-middle">
-                <p className="text-center my-4 text-xs  lg:border-black lg:border-r col-span-1">{member.name}</p>
-                <p className="text-center my-4 text-xs lg:border-black lg:border-r col-span-1">{member.email}</p>
-                <p className="text-center my-4 text-xs lg:border-black lg:border-r col-span-1">{member.id}</p>
-                <p className="text-center my-4 text-xs lg:border-black lg:border-r col-span-1">{member.contact}</p>
-                <p className="text-center my-4 text-xs lg:border-black lg:border-r col-span-1">{member.team} Team</p>
-                <p className="text-center my-4 text-xs lg:border-black lg:border-r col-span-1">{member.department} Department</p>
-                {member.isLead ? <p className="text-center my-4 text-xs lg:border-black lg:border-r">Team Lead</p> : <button className="text-xs bg-blue-400 px-1 py-1 rounded-md active:bg-blue-500 focus:ring-2 focus:ring-blue-600 m-auto w-20" onClick={setLead}>Set Lead</button>}
+                <p className="text-center my-4 text-base  lg:border-black lg:border-r col-span-1">{member.name}</p>
+                <p className="text-center my-4 text-base lg:border-black lg:border-r col-span-1">{member.email}</p>
+                <p className="text-center my-4 text-base lg:border-black lg:border-r col-span-1">{member.id}</p>
+                <p className="text-center my-4 text-base lg:border-black lg:border-r col-span-1">{member.contact}</p>
+                <p className="text-center my-4 text-base lg:border-black lg:border-r col-span-1">{member.team} Team</p>
+                <p className="text-center my-4 text-base lg:border-black lg:border-r col-span-1">{member.department} Department</p>
+                {member.isLead ? <p className="text-center my-4 text-base lg:border-black lg:border-r">Team Lead</p> : <button className="text-base bg-blue-400 px-1 py-1 rounded-md active:bg-blue-500 focus:ring-2 focus:ring-blue-600 m-auto w-20" onClick={setLead}>Set Lead</button>}
                 <div className="flex justify-center items-center">
-                    <button className="text-xs bg-blue-400 px-1 py-1 w-20 rounded-md active:bg-blue-500 focus:ring-2 focus:ring-blue-600 mx-auto my-1" onClick={editEmployee}>Edit</button>
+                    <button className="text-base bg-blue-400 px-1 py-1 w-20 rounded-md active:bg-blue-500 focus:ring-2 focus:ring-blue-600 mx-auto my-1" onClick={editEmployee}>Edit</button>
                 </div>
                 <div className="flex justify-center items-center">
-                    <button className="text-xs bg-red-400 px-1 py-1 w-20 rounded-md active:bg-red-500 focus:ring-2 focus:ring-red-600 mx-auto my-1" onClick={removeEmployee}>Remove</button>
+                    <button className="text-base bg-red-400 px-1 py-1 w-20 rounded-md active:bg-red-500 focus:ring-2 focus:ring-red-600 mx-auto my-1" onClick={removeEmployee}>Remove</button>
                 </div>
             </div>)
     }
--- src/components/SearchCard.js
+++ src/components/SearchCard.js
@@ -5,17 +5,17 @@ function SearchCard(props) {
     return (
         <>
             <div className="mt-6 mb-0 mx-auto bg-yellow-300 shadow-lg rounded-t-xl w-4/5 grid grid-flow-rows sm:grid-rows-2 lg:grid-flow-col px-auto py-2 align-middle">
-                <p className="text-center my-4 text-xs col-span-1">{member.name}</p>
-                <p className="text-center my-4 text-xs col-span-1">{member.email}</p>
-                <p className="text-center my-4 text-xs col-span-1">{member.id}</p>
-                <p className="text-center my-4 text-xs col-span-1">{member.contact}</p>
+                <p className="text-center my-4 text-base col-span-1">{member.name}</p>
+                <p className="text-center my-4 text-base col-span-1">{member.email}</p>
+                <p className="text-center my-4 text-base col-span-1">{member.id}</p>
+                <p className="text-center my-4 text-base col-span-1">{member.contact}</p>
             </div>
             <div className="grid grid-flow-col rounded-b-xl bg-blue-200 shadow-lg mt-0 w-4/5 mx-auto text-center" >
-                {member.isHead && member.department ? <p className="text-center my-4 text-xs  col-span-1">Head of {member.department}</p> : null}
-                {member.isCeo ? <p className="text-center my-4 text-xs  col-span-1">CEO</p> : null}
-                {member.team ? <p className="text-center my-4 text-xs  col-span-1">{member.team} Team</p> : null}
-                {member.isLead ? <p className="text-center my-4 text-xs  col-span-1">Lead of {member.team}</p> : null}
-                {member.department ? <p className="text-center my-4 text-xs  col-span-1">{member.department} Department</p> : null}
+                {member.isHead && member.department ? <p className="text-center my-4 text-base  col-span-1">Head of {member.department}</p> : null}
+                {member.isCeo ? <p className="text-center my-4 text-base  col-span-1">CEO</p> : null}
+                {member.team ? <p className="text-center my-4 text-base  col-span-1">{member.team} Team</p> : null}
+                {member.isLead ? <p className="text-center my-4 text-base  col-span-1">Lead of {member.team}</p> : null}
+                {member.department ? <p className="text-center my-4 text-base  col-span-1">{member.department} Department</p> : null}
             </div>
         </>
     )
